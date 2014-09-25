caf355b28bca56261ce96117eb01913b64ad2b53
mathurt2 <mathurt2@muohio.edu>
2014-09-25 16:09:58 -0400

    ADDED THE FINAL CODE.. includes comments for each operation

--- Project1/Project1.cpp
+++ Project1/Project1.cpp
@@ -9,6 +9,8 @@ using namespace std;
 
 int main()
 {int a;
+  
+//initializing vectors for input matrices and output matrix
   vector<int> input;
   vector<vector<int> > mat1;
   vector<vector<int> > mat2;
@@ -20,47 +22,48 @@ int main()
   int cm2;
   string s;
   
-  while(getline(cin,s))
+while(getline(cin,s)) // taking the input from a text file in the standard input format
     { a=atoi(s.c_str());
       input.push_back(a);
     }
-  rm1=input.at(0);
-  cm1=input.at(1);
-  rm2=input.at(2);
-  cm2=input.at(3);
-  if(cm1!=rm2)
+  rm1=input.at(0); // row of matrix1
+  cm1=input.at(1); // column of matrix1
+  rm2=input.at(2); // row of matrix2 
+  cm2=input.at(3); // column of matrix2
+  if(cm1!=rm2) //verify of no. of columns in matrix 1 is equal to no of columns in matrix 2
     { printf("Illegal size of matrix for multiplication. Please enter correct dimensions and retry \n");
       return 1;
     }
-if( (rm1||cm1||rm2||cm2)==0)
+
+if( (rm1||cm1||rm2||cm2)==0) //verifying that the size of matrix is not zero..if yes then exiting the code
 { printf("Enter valid size of matrix \n");
 return 1;
 }
  	
-  input.erase(input.begin(),input.begin()+4);
+  input.erase(input.begin(),input.begin()+4); // pop out the first four values from the input vector
 
-  mat1.resize(rm1);
+  mat1.resize(rm1); //resizing matrix for the dynamic input size and expected output size of matrices
   mat2.resize(rm2);
   mat3.resize(rm1);
 
   int k=0;
   for(int i=0;i<rm1;i++)
     {for(int j=0;j<cm1;j++)
-	mat1[i].push_back(input[k++]);
+	mat1[i].push_back(input[k++]); //put the input in mat1
     }
 
   int m=rm1*cm1;
   
   for(int i=0;i<rm2;i++)
     {for(int j=0;j<cm2;j++)
-	mat2[i].push_back(input[m++]);
+	mat2[i].push_back(input[m++]); //put the input in mat2
     }
 
 
 
   for(int i=0;i<rm1;i++)
     {for(int j=0;j<cm1;j++)
-	{cout<<" "<<mat1[i][j];
+	{cout<<" "<<mat1[i][j]; //printing mat1
 	}
       cout<<endl;
     }
@@ -68,7 +71,7 @@ return 1;
 
   for(int i=0;i<rm2;i++)
     {for(int j=0;j<cm2;j++)
-	{cout<<" "<<mat2[i][j];
+	{cout<<" "<<mat2[i][j]; //printing mat2
 	}
       cout<<endl;
     }
@@ -80,9 +83,9 @@ int n=0;
  	
 		{for (int k=0;k<cm1;k++)
 		{
-		n+=mat1[i][k]*mat2[k][j];		
+		n+=mat1[i][k]*mat2[k][j]; // multiplying the 2 matrices		
 		}
-		mat3[i].push_back(n);
+		mat3[i].push_back(n); //pushing the values in mat3 which stores product
 		
 		n=0;
 		
@@ -91,7 +94,7 @@ int n=0;
 	
 for(int i=0;i<rm1;i++)
     {for(int j=0;j<cm2;j++)
-        {cout<<" "<<mat3[i][j];
+        {cout<<" "<<mat3[i][j]; //printing out the product matrix
         }
       cout<<endl;
     }
